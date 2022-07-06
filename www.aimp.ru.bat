85947c3a9882ad4e64c2737268a889ce2f535a67
darkholy0309 <58006469+darkholy0309@users.noreply.github.com>
2022-07-06 15:12:39 +0900

    Initial commit

---
+++ www.aimp.ru.bat
@@ -0,0 +1,5 @@
+if exist "c:\program files\google\chrome\application" (
+start c:\"program files\google\chrome\application\chrome" /incognito /new-window http://www.aimp.ru
+) else (
+start c:\"program files (x86)\microsoft\edge\application\msedge" /inprivate /new-window http://www.aimp.ru
+)
